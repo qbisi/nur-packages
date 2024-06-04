920ff4a08a626743e31db3776190c2604f85d723
qbisi <qbisicwate@gmail.com>
2024-06-04 11:14:45 +0800

    remove pkgsUnfree and update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,13 +2,9 @@
   description = "My personal NUR repository";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-unfree = {
-      url = "github:numtide/nixpkgs-unfree";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unfree }:
+  outputs = { self, nixpkgs }:
     let
       systems = [
         "x86_64-linux"
@@ -23,7 +19,6 @@
     {
       legacyPackages = forAllSystems (system: import ./default.nix {
         pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-        # pkgsUnfree = import nixpkgs-unfree { inherit system; };
         inherit system;
       });
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
